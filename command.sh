c22f12363b0c55624efaf7e00aa2f2bf12818656
khr46 <103100607+enitt@users.noreply.github.com>
2023-09-21 15:26:35 +0900

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /usr/local/bin/
-chmod 777 /usr/local/bin/iiot_gw
+chmod 777 /usr/local/bin/iiot_gw_2
 chmod 777 /usr/local/bin/iiot_hdms
 chmod 777 /usr/local/bin/command.sh
 iiot_hdms &
